9e49f507eb3937414f69b0fe905a1bf3943fae2f
Mircea Casapu <mircea.casapu@gmail.com>
2024-02-27 16:50:37 +0200

    chore: scaffold prisma with sqlite db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CalendarModule } from './calendar/calendar.module';
+import { ConfigModule } from '@nestjs/config';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [CalendarModule],
+  imports: [
+    CalendarModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    SharedModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
